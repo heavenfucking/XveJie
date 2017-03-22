1fc34d537f76f996c78cc7c73c1167df4311190c
zhuhao1994 <1536191986@qq.com>
2017-03-22 17:16:00 +0800

    Initial commit

---
+++ src/main/java/com/zhxp/XveJieApplication.java
@@ -0,0 +1,12 @@
+package com.zhxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XveJieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XveJieApplication.class, args);
+	}
+}
